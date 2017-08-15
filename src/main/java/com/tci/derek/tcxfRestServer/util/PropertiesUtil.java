24058467ef5bb83286edc2b247ed2e7c1f5f5165
syuan0321 <syuan0321@gmail.com>
2017-08-15 10:19:39 -0400

    add properties with argument -Denv boots. Majorly is properties file set

---
+++ src/main/java/com/tci/derek/tcxfRestServer/util/PropertiesUtil.java
@@ -0,0 +1,26 @@
+package com.tci.derek.tcxfRestServer.util;
+
+import java.util.Properties;
+
+import org.springframework.beans.factory.InitializingBean;
+
+public class PropertiesUtil implements InitializingBean{
+	private Properties properties;
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+
+	public void setProperties(Properties properties) {
+		this.properties = properties;
+	}
+
+	public void afterPropertiesSet() throws Exception {
+		System.out.println(properties.getProperty("dev.property.path"));
+	}
+	
+	
+	
+	
+}
